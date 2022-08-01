84bb8b6f4dd1e9c86c8eb1f13bbea1d09bcd9b49
DarkFalc0n <unfezant211@gmail.com>
2022-08-01 23:44:36 +0530

    formatting fixed

--- src/components/calendar/Calendar.js
+++ src/components/calendar/Calendar.js
@@ -3,28 +3,47 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import CalendarModeToggler from '../modeToggler/CalendarModeToggler'
 import { useState, useRef, useEffect } from 'react'
+import holidaydata from './2022holidays.json'
 import "./Calendar.css"
+
 
 
 
 const Calendar = () => {
 
     const [events, setEvents] = useState();
+    const [holidays, setHolidays] = useState();
+
+    function parseHolidays(holidays) {
+        const holidayEvents = [];
+        for (let i = 0; i < holidays.length; i++){
+            holidayEvents.push({
+                start: holidays[i].date,
+                title: holidays[i].name,
+            })
+        }
+        return holidayEvents;
+    }
+
     function createEvents(timeslots) {
         const events = [];
         for (let i = 0; i < timeslots.length; i++){
             events.push({
                 startTime: timeslots[i] + ':00',
+                color: "#dc562e",
             })
         }
         return events;
     }
 
     useEffect(async () => {
-        const response = await fetch("http://localhost:3000/time_slot");
-        const timeslots = await response.json();
+        const eventResponse = await fetch("http://localhost:3000/time_slot");
+        const timeslots = await eventResponse.json();
         setEvents(createEvents(timeslots));
+        setHolidays(parseHolidays(holidaydata));
+        console.log(holidaydata);
      }, [])
+     
 
     const calendarRef = useRef();
 
@@ -44,13 +63,14 @@ const Calendar = () => {
             <div className="calendar-wrapper">
                 <CalendarModeToggler onChange = {(mode) => setMode(mode)} mode = {calendarMode}/>
                 <FullCalendar
-                    plugins={[dayGridPlugin]}
+                    plugins={[dayGridPlugin] }
                     initialView={calendarMode}
                     ref={calendarRef}
                     events={calendarMode === "dayGridMonth" ? [{
                         title: `${events.length} slots`,
                         startTime: "00:00:00",
                         allDay: true,
+                        color: "#dc562e",
                     }] : { events }}
                 />
             </div>
